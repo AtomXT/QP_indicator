5ba89ac1e46ef40d604d6f886c9478d5f4b0e947
tongxu <xu-tong@outlook.com>
2026-04-09 18:19:52 -0500

    add results of parametric on path graphs

--- analysis/summarize_Sparsity_Q_path_results.py
+++ analysis/summarize_Sparsity_Q_path_results.py
@@ -26,6 +26,9 @@ def main():
     results_dir = os.path.join(current_dir, "..", "experiments_results")
     df_all = load_results(results_dir)
 
+    df_parametric = pd.read_csv(results_dir + "/parametric_tree_path_Q.csv")
+    df_all = pd.concat([df_all, df_parametric[df_parametric.formulation == 'tree']])
+
     # If it solved in one node then the root gap is zero.
     mask = df_all["node_count"] <= 1
     df_all.loc[mask, ["root_ub", "root_lb"]] = df_all.loc[mask, ["end_ub", "end_lb"]].to_numpy()
@@ -43,24 +46,30 @@ def main():
     n_values = np.array([100, 200, 300, 500, 1000, 2000, 5000, 10000])
     avg = avg.reset_index()
     core_times = avg[avg['formulation'] == 'opt'].time
+    tree_times = avg[avg['formulation'] == 'tree'].time
 
     # Fit slope in log-log space
     log_n = np.log(n_values)
     log_t = np.log(core_times)
+    log_t_parametric = np.log(tree_times)
 
     slope, intercept = np.polyfit(log_n, log_t, 1)
+    slope1, intercept1 = np.polyfit(log_n, log_t_parametric, 1)
 
     print(f"Estimated slope: {slope:.3f}")
 
     plt.figure(figsize=(6, 4))
 
     fitted = np.exp(intercept) * n_values ** slope
-    plt.loglog(n_values, fitted, linestyle='--', label=f'Fit slope={slope:.2f}')
+    fitted1 = np.exp(intercept1) * n_values ** slope1
+    plt.loglog(n_values, fitted, linestyle='--', label=f'Fit slope for CORe={slope:.2f}')
+    plt.loglog(n_values, fitted1, linestyle='--', label=f'Fit slope for parametric={slope1:.2f}')
     plt.loglog(n_values, core_times, marker='o', linewidth=2, label='CORe')
+    plt.loglog(n_values, tree_times, marker='s', linewidth=2, label='parametric')
 
     plt.xlabel('Problem size n (log scale)')
     plt.ylabel('Solution time (seconds, log scale)')
-    plt.title('Log-Log Plot: CORe (Path Graph)')
+    plt.title('Log-Log Plot: CORe vs parametric (Path Graph)')
     plt.grid(True, which="both", linestyle='--', linewidth=0.5)
 
     plt.legend()
