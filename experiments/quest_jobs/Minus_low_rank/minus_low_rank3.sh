1fd03cf9e969d3832e95ea49a15240858e68a3a3
tongxu <xu-tong@outlook.com>
2026-04-09 17:48:11 -0500

    extrac results of diagonal minus low rank

---
+++ experiments/quest_jobs/Minus_low_rank/minus_low_rank3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=p32811  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=8 ## how many cpus or processors do you need on each >
+#SBATCH --time=4:00:00 ## how long does this need to run (remember different p>
+#SBATCH --mem=8G ## how much RAM do you need per CPU, also see --mem=<XX>G for >
+#SBATCH --job-name=diabetes_m5  ## When you run squeue -u NETID this is how you ca>
+#SBATCH --output=quest_jobs/outlog/diagonal_minus_low_rank3_log ## standard out and standar>
+#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your j>
+#SBATCH --mail-user=tongxu2027@u.northwestern.edu ## your email
+
+module purge all
+module load python-miniconda3
+source activate python39
+module load gurobi
+
+python3 -m experiments.Run_diagonal_minus_low_rank \
+  --dataset "diabetes" \
+  --timelimit "3600" \
+  --dimension "5" \
+  --job_name "diabetes_m5"
+
