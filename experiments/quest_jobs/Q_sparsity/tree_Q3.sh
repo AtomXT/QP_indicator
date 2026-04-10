bb7f31c5cb1a36b18f6aae4f314e5cef94412aea
tongxu <xu-tong@outlook.com>
2026-04-10 09:35:41 -0500

    tree experiments results

---
+++ experiments/quest_jobs/Q_sparsity/tree_Q3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=p32811  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=8 ## how many cpus or processors do you need on each >
+#SBATCH --time=12:00:00 ## how long does this need to run (remember different p>
+#SBATCH --mem=12G ## how much RAM do you need per CPU, also see --mem=<XX>G for >
+#SBATCH --job-name=tree_Q3  ## When you run squeue -u NETID this is how you ca>
+#SBATCH --output=quest_jobs/outlog/Q_sparsity_tree_Q3_log ## standard out and standar>
+#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your j>
+#SBATCH --mail-user=tongxu2027@u.northwestern.edu ## your email
+
+module purge all
+module load python-miniconda3
+source activate python39
+module load gurobi
+
+python3 -m experiments.Run_tree_Q \
+  --n_list "100,200,300,500,1000,2000,5000,10000" \
+  --timelimit "600" \
+  --rep_list "2" \
+  --job_name "tree_Q3"
+
