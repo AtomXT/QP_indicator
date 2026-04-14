3a8fb728b99a7fb258bc65a3d334573f81411ea8
tongxu <xu-tong@outlook.com>
2026-04-14 03:04:54 -0500

    single index model results

---
+++ experiments/quest_jobs/single_index_model/robust_binary_classification4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --account=p32811  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=8 ## how many cpus or processors do you need on each >
+#SBATCH --time=3:00:00 ## how long does this need to run (remember different p>
+#SBATCH --mem=8G ## how much RAM do you need per CPU, also see --mem=<XX>G for >
+#SBATCH --job-name=r_bi_4  ## When you run squeue -u NETID this is how you ca>
+#SBATCH --output=quest_jobs/outlog/robust_binary_classification_4_log ## standard out and standar>
+#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your j>
+#SBATCH --mail-user=tongxu2027@u.northwestern.edu ## your email
+
+module purge all
+module load python-miniconda3
+source activate python39
+module load gurobi
+
+python3 -m experiments.Run_single_index_model \
+  --n_list "500" \
+  --timelimit "3600" \
+  --job_name "r_bi_4"
+
