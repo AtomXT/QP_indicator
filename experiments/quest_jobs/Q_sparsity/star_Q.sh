ef2db2388b60b79da03a9d4bed78984a1fac4b69
tongxu <xu-tong@outlook.com>
2026-04-16 21:15:37 -0500

    results of star graph

--- experiments/quest_jobs/Q_sparsity/star_Q.sh
+++ experiments/quest_jobs/Q_sparsity/star_Q.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=8 ## how many cpus or processors do you need on each >
-#SBATCH --time=02:00:00 ## how long does this need to run (remember different p>
+#SBATCH --time=04:00:00 ## how long does this need to run (remember different p>
 #SBATCH --mem=12G ## how much RAM do you need per CPU, also see --mem=<XX>G for >
 #SBATCH --job-name=star_Q  ## When you run squeue -u NETID this is how you ca>
 #SBATCH --output=quest_jobs/outlog/Q_sparsity_star_Q_log ## standard out and standar>
@@ -16,7 +16,7 @@ source activate python39
 module load gurobi
 
 python3 -m experiments.Run_star_Q \
-  --n_list "100,200,300,500,1000,2000,5000,10000" \
+  --n_list "100,200,300,500,1000,2000" \
   --timelimit "600" \
   --rep_list "0,1,2,3,4" \
   --job_name "star_Q"
